00519da59ca0efaec3c697f232f805a671945307
Sean Johnson <sjohnson@sjohnsonaz.com>
2015-06-19 13:26:35 -0700

    Updated DataSource.

--- js/build/service/DataSource.js
+++ js/build/service/DataSource.js
@@ -1,12 +1,27 @@
 /**
  * @class build.service.DataSource
+ * @extends build.Module
  */
-Build('build.service.DataSource', [], function($define, $super) {
+Build('build.service.DataSource', [ 'build::build.Module' ], function($define,
+		$super) {
 	$define({
+		$extends : 'build.Module',
 		/**
 		 * @constructor
 		 */
-		$constructor : function DataSource() {
+		$constructor : function DataSource(service) {
+			$super(this)();
+			this.service = service;
+			this.columns;
+			this.pageSize;
+			this.sortedColumn;
+			this.sortedDirection;
+			this.page;
+			this.rowCount;
+			this.activeRows;
+			this.lockRefresh;
+			this.run;
+			this.clear;
 		}
 	});
 });
