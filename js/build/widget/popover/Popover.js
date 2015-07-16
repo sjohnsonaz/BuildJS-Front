d3099862fbb5c55bbd826ec3a3140a52d547ac3d
Sean Johnson <sjohnson@sjohnsonaz.com>
2015-07-15 17:06:10 -0700

    Updated Popover.

--- js/build/widget/popover/Popover.js
+++ js/build/widget/popover/Popover.js
@@ -5,22 +5,17 @@
 Build('build.widget.popover.Popover', [ 'build::build.ui.Widget' ], function(
 		$define, $super) {
 	$define({
-		$extends : 'build.ui.Widget',
+		$extends : 'build.ui.Container',
 		/**
 		 * @constructor
 		 */
 		$constructor : function Popover() {
 			$super(this)();
-			this.trigger = document.createElement('button');
-			this.trigger.className = 'popover-trigger';
-			this.popover = document.createElement('div');
-			this.popover.className = 'popover';
 			this.content = document.createElement('div');
 			this.content.className = 'popover-content';
-			this.popover.appendChild(this.content);
-			this.trigger.appendChild(this.popover);
-			this.content.innerHTML = 'test';
-			this.element.appendChild(this.trigger);
+			this.innerElement = this.content;
+			this.element.appendChild(this.innerElement);
+			this.element.className = 'popover';
 		}
 	});
 });
