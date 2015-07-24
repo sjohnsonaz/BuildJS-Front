455929d64e999b70a7d40f497e5612c1e3ddddc8
Sean Johnson <sjohnson@sjohnsonaz.com>
2015-07-23 17:07:55 -0700

    Updated DataSource.

--- js/build/service/DataSource.js
+++ js/build/service/DataSource.js
@@ -2,8 +2,7 @@
  * @class build.service.DataSource
  * @extends build.Module
  */
-Build('build.service.DataSource', [ 'build::build.Module',
-		'build::build.utility.ObservableArray' ], function($define, $super) {
+Build('build.service.DataSource', [ 'build::build.Module', 'build::build.utility.ObservableArray' ], function($define, $super) {
 	$define({
 		$extends : 'build.Module',
 		/**
@@ -11,6 +10,7 @@ Build('build.service.DataSource', [ 'build::build.Module',
 		 */
 		$constructor : function DataSource(service) {
 			$super(this)();
+			var self = this;
 			this.watchValue('service', service);
 			this.watchValue('columns', build.utility.ObservableArray());
 			this.watchValue('pageSize', 15);
@@ -20,10 +20,52 @@ Build('build.service.DataSource', [ 'build::build.Module',
 			this.watchValue('rowCount', 0);
 			this.watchValue('activeRows', build.utility.ObservableArray());
 			this.watchValue('lockRefresh', false);
+			var runCount = 0;
 			this.run = function() {
+				runCount++;
+				var runID = runCount;
+				self.lockRefresh = false;
+				self.refreshData(self.page, self.pageSize, self.sortedColumn, self.sortedDirection, function(data) {
+					if (runID == runCount) {
+						self.activeRows = data.Results;
+						self.rowCount = data.ResultCount;
+					}
+				}, function() {
+					if (runID == runCount) {
+						self.activeRows = [];
+						self.rowCount = 0;
+						console.log('refreshData error');
+					}
+				});
 			};
 			this.clear = function() {
+				var lockRefresh = this.lockRefresh;
+				this.page = 0;
+				this.rowCount = 0;
+				this.activeRows = [];
+				this.lockRefresh = lockRefresh;
 			};
+			this.bind([ {
+				handler : 'oneWay',
+				sources : [ {
+					source : this,
+					property : 'pageSize'
+				}, {
+					source : this,
+					property : 'page'
+				}, {
+					source : this,
+					property : 'sortedColumn'
+				}, {
+					source : this,
+					property : 'sortedDirection'
+				} ],
+				output : function(pageSize, page, sortedColumn, sortedDirection) {
+					if (!self.lockRefresh) {
+						self.run();
+					}
+				}
+			} ]);
 		}
 	});
 });
